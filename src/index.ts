4e6e36a09e40ca00004d25edbe70995f00077cfb
joynandi02 <48258123+joynandi02@users.noreply.github.com>
2019-03-08 18:44:43 +0000

    First pass

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
 import './styles.css';
 console.log('Ready to Party');
+
+const radios = document.querySelectorAll('input[type="radio"]');
+console.log(radios)
+radios.forEach(s => s.addEventListener('click', calculate))
+
+
+const numberInput: HTMLInputElement = <HTMLInputElement>document.getElementById("bill");
+const tipInput: HTMLInputElement = <HTMLInputElement>document.getElementById("option1");
+
+const billAmount: HTMLInputElement = <HTMLInputElement>document.getElementById("billAmount");
+const tipPercentage: HTMLInputElement = <HTMLInputElement>document.getElementById("tipPercentage");
+const tipAmount: HTMLInputElement = <HTMLInputElement>document.getElementById("tipAmount");
+const totalAmount: HTMLInputElement = <HTMLInputElement>document.getElementById("totalAmount");
+
+function calculate() {
+    const numberinputString = numberInput.value;
+    const cost = parseFloat(numberinputString);
+    const tip = cost * parseFloat(tipInput.value);
+    const total = cost + tip;
+
+    billAmount.innerText = billAmount.innerText + " " + numberInput.value;
+    tipPercentage.innerText = tipPercentage.innerText + " " + tipInput.value;
+    tipAmount.innerText = tipAmount.innerText + " " + tip.toString();
+    totalAmount.innerText = totalAmount.innerText + " " + total.toString();
+}
