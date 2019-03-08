97a36c112222e1d527e2ae098b12dff337f89729
joynandi02 <48258123+joynandi02@users.noreply.github.com>
2019-03-08 20:19:18 +0000

    Second pass

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,28 @@ import './styles.css';
 console.log('Ready to Party');
 
 const radios = document.querySelectorAll('input[type="radio"]');
-console.log(radios)
 radios.forEach(s => s.addEventListener('click', calculate))
 
 
 const numberInput: HTMLInputElement = <HTMLInputElement>document.getElementById("bill");
-const tipInput: HTMLInputElement = <HTMLInputElement>document.getElementById("option1");
 
 const billAmount: HTMLInputElement = <HTMLInputElement>document.getElementById("billAmount");
 const tipPercentage: HTMLInputElement = <HTMLInputElement>document.getElementById("tipPercentage");
 const tipAmount: HTMLInputElement = <HTMLInputElement>document.getElementById("tipAmount");
 const totalAmount: HTMLInputElement = <HTMLInputElement>document.getElementById("totalAmount");
+const tipText: HTMLInputElement = <HTMLInputElement>document.getElementById("tip");
 
 function calculate() {
+    const label = this as HTMLInputElement;
+    const tipInput: string = label.value;
     const numberinputString = numberInput.value;
     const cost = parseFloat(numberinputString);
-    const tip = cost * parseFloat(tipInput.value);
+    const tip = cost * parseFloat(tipInput);
     const total = cost + tip;
 
-    billAmount.innerText = billAmount.innerText + " " + numberInput.value;
-    tipPercentage.innerText = tipPercentage.innerText + " " + tipInput.value;
-    tipAmount.innerText = tipAmount.innerText + " " + tip.toString();
-    totalAmount.innerText = totalAmount.innerText + " " + total.toString();
+    billAmount.innerText = billAmount.innerText + " $" + numberInput.value;
+    tipPercentage.innerText = tipPercentage.innerText + " " + (parseFloat(tipInput) * 100) + "%";
+    tipAmount.innerText = tipAmount.innerText + " $" + tip.toString();
+    totalAmount.innerText = totalAmount.innerText + " $" + total.toString();
+    tipText.innerText = tipText.innerText + " " + (parseFloat(tipInput) * 100) + "%";
 }
